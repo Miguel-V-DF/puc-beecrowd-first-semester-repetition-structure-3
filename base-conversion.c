a8c08fee4a3794a781bcd02d036264c353312a80
Miguel Vitta <miguelvittadev@gmail.com>
2024-09-29 09:57:53 -0300

    solved the base-conversion problem

--- base-conversion.c
+++ base-conversion.c
@@ -4,21 +4,18 @@
 
 int main()
 {
-    int n;
-    long int resultDecimal = 0;
+    int n, remainder;
+    long int resultDecimal;
     char nbrO[100], text[10];
     
     scanf("%d", &n);
     for(int i = 1; i <= n; i++)
     {
+        resultDecimal = 0;
         scanf("%99s %9s", nbrO, text);
         if(strcmp(text, "bin") == 0)
         {
-            int length = 0;
-            while(nbrO[length] != '\0')
-            {
-                length++;
-            }
+            int length = strlen(nbrO);
             for(int j = 0; j < length; j++)
             {
                 int bit = nbrO[j] - '0';
@@ -34,11 +31,53 @@ int main()
             if(strcmp(text, "hex") == 0)
             {
                 resultDecimal = strtol(nbrO, NULL, 16);
+                printf("Case %d:\n", i);
                 printf("%ld dec\n", resultDecimal);
+                memset(nbrO, '\0', sizeof(nbrO)); 
+                int j = 0;
+                while(resultDecimal > 0)    
+                {
+                    remainder = resultDecimal % 2;
+                    nbrO[j++] = remainder + '0';
+                    resultDecimal = resultDecimal / 2;
+                }
+                nbrO[j] = '\0';
+
+                for(int start = 0, end = j - 1; start < end; start++, end--)
+                {
+                    char temp = nbrO[start];
+                    nbrO[start] = nbrO[end];
+                    nbrO[end] = temp;
+                }
+                printf("%s bin\n", nbrO);
+                printf("\n");
             }
+            else
+            {
+                resultDecimal = strtol(nbrO, NULL, 10);
+                printf("Case %d:\n", i);
+                printf("%x hex\n", resultDecimal);
+                memset(nbrO, '\0', sizeof(nbrO)); 
+                int j = 0;
+                while(resultDecimal > 0)    
+                {
+                    remainder = resultDecimal % 2;
+                    nbrO[j++] = remainder + '0';
+                    resultDecimal = resultDecimal / 2;
+                }
+                nbrO[j] = '\0';
+
+                for(int start = 0, end = j - 1; start < end; start++, end--)
+                {
+                    char temp = nbrO[start];
+                    nbrO[start] = nbrO[end];
+                    nbrO[end] = temp;
+                }
+                printf("%s bin\n", nbrO);
+                printf("\n");
+            }
+
         }
-
     }
-
     return 0;
 }
