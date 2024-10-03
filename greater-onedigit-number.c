8c5abdcdee858f9611f876dd26d8e5b8ce106ca8
Miguel Vitta <miguelvittadev@gmail.com>
2024-10-03 14:07:24 -0300

    trying to debug this problem

--- greater-onedigit-number.c
+++ greater-onedigit-number.c
@@ -2,76 +2,66 @@
 
 int main()
 {
-    int n, m, sumN = 0, sumM = 0, repeater = 0;
+    unsigned long long n, m;
 
-    scanf("%d %d", &n, &m);
-
-    do
+    while(scanf("%llu %llu", &n, &m), !(n == 0 && m == 0))
     {
-        while(n > 0)
+        unsigned long long sumN, sumM;
+        do
         {
-            printf("----------\nstart of the loop\n");
-            sumN = sumN + n % 10;
-            printf("sumN: %d\n", sumN);
-            n = n / 10;
-            printf("n: %d\n", n);
-            printf("----------\nend of the loop\n");
-        }
-        printf("----------\nfinal numbers\n");
-        printf("n: %d\n", n);
-        printf("sumN: %d\n", sumN);
-        if(sumN > 9) 
-        {
-            repeater == 1;
-            n = sumN;
             sumN = 0;
+            printf("sumN initial value: %llu\n", sumN);
+            while(n > 0)
+            {
+                printf("----------\nstart of the loop\n");
+                sumN = sumN + n % 10;
+                printf("sumN: %llu\n", sumN);
+                n = n / 10;
+                printf("n: %llu\n", n);
+                printf("----------\nend of the loop\n");
+            }
+            printf("----------\nfinal numbers\n");
+            printf("n: %llu\n", n);
+            printf("sumN: %llu\n", sumN);
+            n = sumN;
+            printf("n taking sumN: %llu\n", n);
         }
-        else
+        while(sumN > 9);
+        do
         {
-            repeater = 0;
-        }
-    }
-    while(repeater == 1);
-
-    do
-    {
-        while(m > 0)
-        {
-            printf("----------\nstart of the loop\n");
-            sumM = sumM + m % 10;
-            printf("sumM: %d\n", sumM);
-            m = m / 10;
-            printf("m: %d\n", m);
-            printf("----------\nend of the loop\n");
-        }
-        printf("----------\nfinal numbers\n");
-        printf("m :%d\n", m);
-        printf("sumM: %d\n", sumM);
-        if(sumM > 9) 
-        {
-            repeater == 1;
-            m = sumM;
             sumM = 0;
+            printf("sumM initial value: %llu\n", sumM);
+            while(m > 0)
+            {
+                printf("----------\nstart of the loop\n");
+                sumM = sumM + m % 10;
+                printf("sumM: %llu\n", sumM);
+                m = m / 10;
+                printf("m: %llu\n", m);
+                printf("----------\nend of the loop\n");
+            }
+            printf("----------\nfinal numbers\n");
+            printf("M: %llu\n", m);
+            printf("sumM: %llu\n", sumM);
+            m = sumM;
+            printf("m taking sumM: %llu\n", m);
+        }
+        while(sumM > 9);
+
+        if(sumN > sumM)
+        {
+            printf("1\n");
         }
         else
         {
-            repeater = 0;
-        }
-    }
-    while(repeater == 1);
-    if(sumN > sumM)
-    {
-        printf("1\n");
-    }
-    else
-    {
-        if(sumN < sumM)
-        {
-            printf("2\n");
-        }
-        else
-        {
-            printf("0\n");
+            if(sumN < sumM)
+            {
+                printf("2\n");
+            }
+            else
+            {
+                printf("0\n");
+            }
         }
     }
     return 0;
